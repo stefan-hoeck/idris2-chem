a98ce6fd8f5b0087fa8d28018cbb3e4dcc873944
stefan-hoeck <hock@zhaw.ch>
2023-09-24 10:29:14 +0200

    [ new, wip ] added sed script for extracting isotope data

--- generate_data.sh
+++ generate_data.sh
@@ -2,4 +2,5 @@
 
 mkdir -vp build
 sed -nf "chem-data/elements.sed" "resources/elements.xml" > "build/elem.txt"
+sed -nf "chem-data/isotopes.sed" "resources/isotopes.xml" > "build/isotopes.txt"
 pack run chem-data
